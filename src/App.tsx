990f6e4bf6726cc774d58aca9c2c8c8e528efeae
Adam Wayne Carver <adamwaynecarver@Adams-MBP.attlocal.net>
2024-11-23 22:26:29 -0500

    Game Stats Api Fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import HeroHeader from './components/HeroHeader';
 import Grow from './components/Grow';
 import Basketball from './components/Basketball';
 import LittleThings from './components/LittleThings';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="relative App">
       <HeroHeader />
